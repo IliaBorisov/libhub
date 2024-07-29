360c46be0e498b164761c663abfbd81d3863b63f
Ilia Borisov <iliqborisov@gmail.com>
2024-07-29 08:18:50 +0300

    minor changes on BookListItem

--- client/src/components/books-list/book-list-item/BookListItem.jsx
+++ client/src/components/books-list/book-list-item/BookListItem.jsx
@@ -2,20 +2,23 @@ import { Link } from 'react-router-dom';
 
 import './BookListItem.css'
 
-const BOOK_URL = '/none';
+const BOOK_URL = '/book';
+const AUTHOR_URL = '/author';
 
 export default function BookListItem({
     _id,
     title,
     author,
     imageUrl,
+    rating,
 }) {
     return (
         <section className="book-item">
             <Link to={BOOK_URL}><img src={imageUrl} alt="no image" /></Link>
-            <div>
+            <div className='book-item-details'>
                 <Link to={BOOK_URL} >{title}</Link>
-                <p>by {author}</p>
+                <p>by <Link to={AUTHOR_URL}>{author}</Link></p>
+                <p>{rating}</p>
             </div>
         </section>
     );
